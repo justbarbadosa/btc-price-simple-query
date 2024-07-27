b9eff387b90a0031e56f9eefe0de7090ef6bd14e
justbarbadosa <63874636+justbarbadosa@users.noreply.github.com>
2024-07-27 17:15:52 +0300

    Update getBTCPrice.js

--- api/getBTCPrice.js
+++ api/getBTCPrice.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
     const response = await fetch('https://blockchain.info/frombtc?value=10000&currency=USD&textual=true&nosavecurrency=true');
     const data = await response.text();
-    const formattedData = data.replace(/\.\d+Z/, 'Z'); // Remove decimal part from the timestamp
+    // Format the timestamp
+    const formattedData = data.replace(/@(.*)T(.*)\..*Z/, '@$1 $2'); // Remove decimal part and reformat
     res.status(200).send(formattedData);
 };
