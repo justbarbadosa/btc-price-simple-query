a7a60a02693d533e9b4f253edbc6c772cb41fbfa
justbarbadosa <63874636+justbarbadosa@users.noreply.github.com>
2024-08-20 23:38:44 +0300

    Update livestream.js

--- api/livestream.js
+++ api/livestream.js
@@ -1,23 +1,20 @@
+const puppeteer = require('puppeteer');
+
 export default async function handler(req, res) {
   const channelSlug = 'anthonyvizoso';
   const apiUrl = `https://kick.com/api/v2/channels/${channelSlug}`;
 
   try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        'Content-Type': 'application/json',
-        // Add a user-agent header to mimic a browser request
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-      },
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(apiUrl, { waitUntil: 'networkidle2' });
+
+    const data = await page.evaluate(() => {
+      return JSON.parse(document.body.innerText);
     });
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
+    await browser.close();
 
-    const data = await response.json();
-
-    // Process the data as needed and return it
     res.status(200).json(data);
   } catch (error) {
     console.error('Error fetching data:', error);
